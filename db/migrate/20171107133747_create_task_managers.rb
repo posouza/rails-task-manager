9af7442013d2435e40069124bc130a965466ef60
Pedro Souza <posouza@gmail.com>
2017-11-07 11:43:13 -0200

    Add Task Manager model

---
+++ db/migrate/20171107133747_create_task_managers.rb
@@ -0,0 +1,11 @@
+class CreateTaskManagers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :task_managers do |t|
+      t.string :description
+      t.date :deadline
+      t.boolean :done
+
+      t.timestamps
+    end
+  end
+end
